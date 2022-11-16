fd4c3e0a919ceaa900c2ea0a11b5dda7606ba50b
Max <inflatableanchor@gmail.com>
2022-11-16 11:57:52 -0600

    NRPG copied content

---
+++ js/data/episodeListData.js
@@ -0,0 +1,125 @@
+const episodeData = [
+  {
+    title: "Episode 17",
+    date: "February 28th, 2019",
+    description: "<p>	The gang decides to make the Gut stronger by focusing on an ark project: Zone Wrestling.  However, right when things are about to heat up inside the ring, something else heats up outside the walls...</p><p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/8832170/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 16",
+    date: "February 14th, 2019",
+    description: "<p>The Ark has had time to recover and grow since the snafu with Boss Aquifee. Meanwhile, Lefty, Corny, and Pavlovena are given an offer they can't refuse...</p> <p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/8642780/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 15",
+    date: "January 31st, 2019",
+    description: "<p>The mutants are still engrossed in a battle with Aquifee's enforcers, and who will ultimately end up with control of the water resource is anyone's guess. See how this, and everything else, unfolds in this Season Finale.</p> <p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/8465468/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 14",
+    date: "January 19th, 2019",
+    description: "<p>The only thing standing in between the Mutants and Boss Aquifi is a door and lack of a solid plan.  Will they get past the door?  Will they face off with Aquifi once and for all and save the Gut? Will they even survive??  Why are you still reading? Listen and find out, dangit!</p> <p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/8321213/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 13",
+    date: "January 9th, 2019",
+    description: "<p>Things continue to heat up as Corny remains steadfast in his new found toughness, secrets of Pavlovena's love life get uncovered, and Lefty... continues to have two left feet!</p> <p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/8185394/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 12",
+    date: "December 13th, 2018",
+    description: "<p>The gang develops schemes on how to covertly enter the Gut now that Aquifi has taken charge. They receive a special jury-rigged item from their gearhead friends before entering, and altercations ensue.</p> <p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7913252/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 11",
+    date: "December 4th, 2018",
+    description: "<p>Face off!...or face on?</p><p>The gang tries their plan of floating the generator across the river in the jury-rigged Paul Bunyan statue boat, while also finding a unique solution to entering the Ark without Reptillia (they rip his face off and put it on a zone ghoul).  Meanwhile, things have been heating up in the Ark while they were away.</p> <p>Music and pruduction by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7792721/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 10",
+    date: "November 15, 2018",
+    description: "<p>With the threat of Reptilia now neutralized, the gang faces a new obstacle: a group of 8 zone ghouls. Things may not be what they seem though as they continue to learn more about the wasteland beyond the walls of the Gut.</p> <p>Music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7561586/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Spooky Halloween Episode!",
+    date: "October 31, 2018",
+    description: "<p>Meghan Wolff (Co-Host of Good Luck High Five) joins our characters as we play this one shot using Dread RPG System!</p> <p>Lefty learned of powerful artifacts being kept in a far away sector on the zone map.  He enlisted the help of Pavlovena and Chip Carmichael to help retrieve them. </p> <p>Music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573226/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 9",
+    date: "October 18, 2018",
+    description: "<p>  The gang learns about their new gearhead allies, as well as newer more fearsome threats in the zone to look out for.  Meanwhile, Reptilia, and for some reason Corny, continue to try to kill Weezle.  What will it be like to bring gearheads back to a community that's in the middle of a purge?</p><p>Music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573229/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 8",
+    date: "October 4, 2018",
+    description: "<p>An assembly has called for the retrieval of the generator from the theater glowing with devil lights.  However, being forced to team with murderous Reptilia could prove to be difficult.</p> <p> Music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573232/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 7",
+    date: "September 20, 2018",
+    description: "<p>The gang deposits their artifacts to the Dawn Vault, and calls an assembly for a special Zone expedition.  Meanwhile, tensions are on the rise between Boss Aquifi and the gearheads of the Ark. </p><p>Music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573235/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 6",
+    date: "September 6, 2018",
+    description: "<p>Torn between whether to return to the Ark or keep exploring the Zone for more artifacts, the gang opts for more sweet loot.  They also learn that there’s more threats in the Zone than just Zone Ghouls when nature tests them.</p><p>All music composed by Max Beyer</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573238/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 5",
+    date: "August 24, 2018",
+    description: "<p>A mysterious building that glows with devil lights is found in the zone and explored.  Pavlovena gives Crisp special orders, Corny identifies electronic wonders, and Lefty gets distracted hunting a beast through dark hallways.</p><p>Original music composed by Max Beyer.</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573241/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 4",
+    date: "August 9, 2018",
+    description: "<p>Lefty, Corny, and Pavlovena venture into the Zone.  They cross a bridge and find their friend Weezle is in trouble.  Meanwhile, a building nearby somehow has electricity.</p><p>Original music composed by Max Beyer, and a big thanks to Neehnahw from freesound.org for the use of their work Konflict.</p>",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573244/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 3",
+    date: "July 27, 2018",
+    description: "Corny Falco gets stuck in his suit, Pavlovena has a run in with her favorite enforcer, and Lefty feels guilty after pocketing the direction finder instead of securing it in the Dawn Vault. He attempts to recruit his friends to venture out into the Zone to find more items of value to make up for his perceived selfishness.",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573247/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 2",
+    date: "July 12, 2018",
+    description: "Lefty wonders about the possibilities of an artifact he found, but he needs his friends Corny Falco and Pavlovena to help identify what it is and what it does. Meanwhile the people of the Ark decide to build defenses to protect against the dangers of The Zone, when trouble strikes!<br><br>Original music composed by Max Beyer, and a big thanks to these other artists for their music found at freesound.org:<br><br>Sirkoto51 - Anime Fight Music Loop <br><br> neehnahw - Konflict",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573253/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  },
+  {
+    title: "Episode 1",
+    date: "July 12, 2018",
+    description: "In this setup episode of The Notorious RPG, 4 Minneapolis friends and improvisers create their mutant characters in a dystopian post-apocalyptic future.  Join in the campaign as Patrick (Lefty), Maggie (Pavlovena) and Brandon (Corny Falco), led by Game Master Max, venture into the unknown.<br><br>Original music composed by Max Beyer.",
+    audio: `<iframe style="border: none" src="//html5-player.libsyn.com/embed/episode/id/7573256/height/90/theme/custom/autoplay/no/autonext/no/thumbnail/no/preload/no/no_addthis/no/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>`
+  }
+];
+
+function episodeTemplate(episode) {
+  return `
+      <hr class="darker">
+      <h3>${episode.title}</h3>
+      <p><i>${episode.date}</i></p>
+      <p>${episode.description}</p>
+      <p>${episode.audio}</p>
+  `;
+}
+
+document.getElementById("episode-template").innerHTML = `
+   ${episodeData.map(episodeTemplate).join("")}
+  `
+  ;
